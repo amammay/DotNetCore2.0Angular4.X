bbe52488bd890f53c54a1d8d9f483f3db2544032
Alex Mammay <alexmammay@gmail.com>
2017-08-18 00:29:58 -0400

    Updated angular

--- PlayGround/ClientApp/app/components/home/home.component.ts
+++ PlayGround/ClientApp/app/components/home/home.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+    onSubmit(f: NgForm) {
+        console.log(f.value);  // { first: '', last: '' }
+        console.log(f.valid);  // false
+    }
 }
