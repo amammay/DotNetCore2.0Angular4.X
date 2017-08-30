ec8daabb8a05393fba09850153b1c1515b65825f
Alex Mammay <alexmammay@gmail.com>
2017-08-29 21:20:40 -0400

    Additional Updates

--- PsuApi.Tests/Helper.cs
+++ PsuApi.Tests/Helper.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Moq;
+using Moq.Protected;
 
 namespace PsuApi.Tests
 {
@@ -12,6 +16,12 @@ namespace PsuApi.Tests
         public static PsuSearchClient CreateClient(Func<HttpResponseMessage>,
             Action<HttpRequestMessage, CancellationToken> verificationCallback)
         {
+
+            var handler = new Mock<HttpMessageHandler>();
+            handler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync")
+
+
 
             return null;
         }
